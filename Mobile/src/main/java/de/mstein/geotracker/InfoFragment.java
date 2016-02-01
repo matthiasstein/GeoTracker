510e5fcf9f36712f01039452a0aff0936a215e17
Matthias Stein <m.stein@conterra.de>
2016-02-01 22:05:10 +0100

    change info-fragment

--- Mobile/src/main/java/de/mstein/geotracker/InfoFragment.java
+++ Mobile/src/main/java/de/mstein/geotracker/InfoFragment.java
@@ -23,7 +23,7 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return inflater.inflate(R.layout.fragment_info, container, false);
     }
 
 
